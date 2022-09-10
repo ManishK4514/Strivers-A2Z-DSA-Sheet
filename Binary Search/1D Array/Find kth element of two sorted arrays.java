ebc0795865fd86048a14056ab0593c6d9cbed1c6
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2022-09-10 18:04:40 +0530

    Updated Find Kth element in two sorted array.
    This is the program to Find Kth element in two sorted arrays.

--- Binary Search/1D Array/Find kth element of two sorted arrays.java
+++ Binary Search/1D Array/Find kth element of two sorted arrays.java
@@ -42,10 +42,10 @@ public class Kth_element_of_two_sorted_Arrays {
          * Bruteforce Approach: Time complexity: o(N) & Space complexity: O(m + n).
          * int[] nums = new int[m + n];
          * int j = 0;
-         * for(int i = 0; i < n; i++){
+         * for(int i = 0; i < m; i++){
          * nums[j++] = array1[i];
          * }
-         * for(int i = 0; i < m; i++){
+         * for(int i = 0; i < n; i++){
          * nums[j++] = array2[i];
          * }
          * Arrays.sort(nums);
@@ -53,41 +53,69 @@ public class Kth_element_of_two_sorted_Arrays {
          */
         
          /*
-          * Solution 2: Time complex
-          */
-        int i = 0, j = 0;
-        int count = 0;
-        while (i < m && j < n) {
-            if (array1[i] <= array2[j]) {
+          * Solution 2: Time complexity: O(m + n) & Space complexity: O(1).
+            int i = 0, j = 0;
+            int count = 0;
+            while (i < m && j < n) {
+                if (array1[i] <= array2[j]) {
+                    count++;
+                    if (count == k) {
+                        return array1[i];
+                    }
+                    i++;
+                } else {
+                    count++;
+                    if (count == k) {
+                        return array2[j];
+                    }
+                    j++;
+                }
+    
+            }
+            while (i < m) {
                 count++;
                 if (count == k) {
                     return array1[i];
                 }
                 i++;
-            } else {
+            }
+            while (j < n) {
                 count++;
                 if (count == k) {
                     return array2[j];
                 }
                 j++;
             }
+            return -1;
+        */
+        
+        // Optimized Approach: Time Complexity: O(log(min(m,n))) & Space complexity: O(1).
+        if(n > m){
+            return kthelement(array2, array1, n, m, k);
+        }
+        int low = Math.max(0,k-n), high = Math.min(k,m);
+        while(low <= high){
+            int cut1 = low + (high - low)/2;
+            int cut2 = k - cut1;
 
-        }
-        while (i < m) {
-            count++;
-            if (count == k) {
-                return array1[i];
+            // edge cases
+
+            int l1 = cut1 == 0 ? Integer.MIN_VALUE : array1[cut1 - 1]; 
+            int l2 = cut2 == 0 ? Integer.MIN_VALUE : array2[cut2 - 1];
+            int r1 = cut1 == m ? Integer.MAX_VALUE : array1[cut1]; 
+            int r2 = cut2 == n ? Integer.MAX_VALUE : array2[cut2]; 
+            
+            if(l1 <= r2 && l2 <= r1){
+                return Math.max(l1, l2);
             }
-            i++;
-        }
-        while (j < n) {
-            count++;
-            if (count == k) {
-                return array2[j];
+            else if(l1 > r2){
+                high = cut1 - 1;
             }
-            j++;
+            else{
+                low = cut1 + 1;
+            }
         }
-        return -1;
+        return 1;
     }
 
     public static void main(String[] args) {
