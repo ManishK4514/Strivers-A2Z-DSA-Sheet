809f55597165095f5ff2ff7ff66cb76bd81b7fa2
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2022-09-17 17:29:54 +0530

    Updated Longest Common Prefix.

--- Strings/easy/Longest Common Prefix.java
+++ Strings/easy/Longest Common Prefix.java
@@ -21,19 +21,34 @@ public class Longest_Common_Prefix {
     public String longestCommonPrefix(String[] strs) {
         /*
          * BruteForce Approach: Time complexity: O(firstStringLength * N) & Space complexity: O(1).
-         */
+         * String str = strs[0];
+           String finalAns = "";
+           for(int i = 0; i < str.length(); i++){
+               for(int j = 0; j < strs.length; j++){
+                   String curr = strs[j];
+                   if(i + 1> curr.length() || str.charAt(i) != curr.charAt(i)){
+                       return finalAns;
+                   }
+               }
+               finalAns += str.charAt(i);
+           }
+           return finalAns;
+        */
+        
+        // Optimized: Used StringBuilder 
+        
         String str = strs[0];
-        String finalAns = "";
+        StringBuilder finalAns = new StringBuilder();
         for(int i = 0; i < str.length(); i++){
             for(int j = 0; j < strs.length; j++){
                 String curr = strs[j];
                 if(i + 1> curr.length() || str.charAt(i) != curr.charAt(i)){
-                    return finalAns;
+                    return finalAns.toString();
                 }
             }
-            finalAns += str.charAt(i);
+            finalAns.append(str.charAt(i));
         }
-        return finalAns;
+        return finalAns.toString();
     }
     public static void main(String[] args) {
         
