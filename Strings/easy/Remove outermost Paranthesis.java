b5be8f14e92d07237163a0449a0dd23001b6a03c
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2022-09-17 16:27:47 +0530

    Updated Remove Outermost Parentheses.

--- Strings/easy/Remove outermost Paranthesis.java
+++ Strings/easy/Remove outermost Paranthesis.java
@@ -41,21 +41,42 @@
 
 public class Remove_Outermost_Parentheses {
     public static String removeOuterParentheses(String str) {
+        /* 
+           Time complexity: O(N^2) & Space complexity: O(1). 
+           N^2 due to every time creating a new string object
+
+           String ans = "";
+           int j = 0;
+           for(int i = 0; i < str.length() - 1; i++){
+               if(str.charAt(i) == ')'){
+                   j--;
+               }
+               if(j != 0){
+                   ans += str.charAt(i);
+               }
+               if(str.charAt(i) == '('){
+                   j++;
+               }
+           }
+           return ans;
+        */
+ 
         // Time complexity: O(N) & Space complexity: O(1).
-        String ans = "";
+
+        StringBuilder ans = new StringBuilder();
         int j = 0;
         for(int i = 0; i < str.length() - 1; i++){
             if(str.charAt(i) == ')'){
                 j--;
             }
             if(j != 0){
-                ans += str.charAt(i);
+                ans.append(str.charAt(i));
             }
             if(str.charAt(i) == '('){
                 j++;
             }
         }
-        return ans;
+        return ans.toString();
     }
     public static void main(String[] args) {
         String str = "(()())(())";
