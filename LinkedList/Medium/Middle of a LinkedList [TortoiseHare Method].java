cdf0e903f30abee5faaba83177d4420c84c41b0f
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2022-09-23 22:33:06 +0530

    Updated middle of the LinkedList.

--- LinkedList/Medium/Middle of a LinkedList [TortoiseHare Method].java
+++ LinkedList/Medium/Middle of a LinkedList [TortoiseHare Method].java
@@ -8,6 +8,27 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+/*
+  class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode node = head;
+        int count = 0;
+        while(node != null){
+            count++;
+            node = node.next;
+        }
+        int index = count/2;
+        node = head;
+        for(int i = 0; i < index; i++){
+            node = node.next;
+        }
+        return node;
+    }
+}
+
+*/
+
 class Solution {
     public ListNode middleNode(ListNode head) {
         ListNode slow = head, fast = head;
