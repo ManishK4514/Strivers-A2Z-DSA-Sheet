a60f64bebd53548bed019e60fc0cebc41853c3d4
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2022-09-23 22:48:23 +0530

    Added Remove Nth node from the back of the LL.
    This is the program to Remove Nth node from the back of the LL.

---
+++ LinkedList/Medium/Remove Nth node from the back of the LL.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode start = new ListNode();
+        start.next = head;
+        ListNode slow = start;
+        ListNode fast = start;
+        for(int i = 1; i <= n; i++){
+            fast = fast.next;
+        }
+        while(fast.next != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return start.next;
+    }
+}
