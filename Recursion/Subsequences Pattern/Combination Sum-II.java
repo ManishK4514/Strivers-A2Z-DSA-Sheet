d1054f05e5ffe81a11768d19ed711eaa6f52a6a8
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2022-10-06 18:14:26 +0530

    Updated Combination Sum-II.

--- Recursion/Subsequences Pattern/Combination Sum-II.java
+++ Recursion/Subsequences Pattern/Combination Sum-II.java
@@ -23,27 +23,58 @@
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Combination_Sum_II {
-    public static void findCombination(int idx, int[] arr, int target, List<List<Integer>> ans, ArrayList<Integer> ds){
-        if(idx == arr.length){
-            if(target == 0){
-                if(!ans.contains(ds)){
-                    ans.add(new ArrayList<>(ds));
+    /*
+        BruteForce Approach: Time complexity: O(2^n) * k * logN.
+
+        public static void findCombination(int idx, int[] arr, int target, List<List<Integer>> ans, ArrayList<Integer> ds){
+            if(idx == arr.length){
+                if(target == 0){
+                    if(!ans.contains(ds)){
+                        ans.add(new ArrayList<>(ds));
+                    }
                 }
+                return;
+            }
+            // will take
+           
+            ds.add(arr[idx]);
+            findCombination(idx + 1, arr, target - arr[idx], ans, ds);
+            ds.remove(ds.size() - 1);
+    
+            // will not take
+            findCombination(idx + 1, arr, target, ans, ds);
+        }
+        public static List<List<Integer>> combinationSum(int[] arr, int target) {
+            List<List<Integer>> ans = new ArrayList<>();
+            findCombination(0, arr, target, ans, new ArrayList<>());
+            return ans;
+        }
+
+    */
+
+    // Solution 2: Time complexity: O(2^n) * k. 
+
+    public static void findCombination(int idx, int[] arr, int target, List<List<Integer>> ans, ArrayList<Integer> ds){
+        if(target == 0){
+            if(!ans.contains(ds)){
+                ans.add(new ArrayList<>(ds));
             }
             return;
         }
-        // will take
-       
-        ds.add(arr[idx]);
-        findCombination(idx + 1, arr, target - arr[idx], ans, ds);
-        ds.remove(ds.size() - 1);
+        for(int i = idx; i < arr.length; i++){
+            if(i > idx && arr[i] == arr[i - 1]) continue;
 
-        // will not take
-        findCombination(idx + 1, arr, target, ans, ds);
+            if(arr[i] > target) break;
+            ds.add(arr[i]);
+            findCombination(i + 1, arr, target - arr[i], ans, ds);
+            ds.remove(ds.size() - 1);
+        }
     }
     public static List<List<Integer>> combinationSum(int[] arr, int target) {
+        Arrays.sort(arr);
         List<List<Integer>> ans = new ArrayList<>();
         findCombination(0, arr, target, ans, new ArrayList<>());
         return ans;
