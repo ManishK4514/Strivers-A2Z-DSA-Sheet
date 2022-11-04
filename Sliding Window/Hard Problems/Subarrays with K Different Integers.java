a8623541db311ac56c12431c9375e4f18d26e3ac
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2022-11-04 22:25:38 +0530

    Added Subarrays with K Different Integers.

---
+++ Sliding Window/Hard Problems/Subarrays with K Different Integers.java
@@ -0,0 +1,77 @@
+/*
+   Given an integer array nums and an integer k, return the number of good subarrays of nums.
+
+   A good array is an array where the number of different integers in that array is exactly k.
+   
+   For example, [1,2,3,1,2] has 3 different integers: 1, 2, and 3.
+   A subarray is a contiguous part of an array.
+   
+   Example 1:   
+   Input: nums = [1,2,1,2,3], k = 2
+   Output: 7
+   Explanation: Subarrays formed with exactly 2 different integers: [1,2], [2,1], [1,2], [2,3], [1,2,1], [2,1,2], [1,2,1,2]
+
+   Example 2:   
+   Input: nums = [1,2,1,3,4], k = 3
+   Output: 3
+   Explanation: Subarrays formed with exactly 3 different integers: [1,2,1,3], [2,1,3], [1,3,4].
+*/
+
+
+// import java.util.HashSet;
+import java.util.HashMap;
+
+public class Subarrays_with_K_Different_Integers {
+    public static int subarraysWithKDistinct(int[] nums, int k) {
+        /* 
+            // BruteForce Approach: Time Complexity: O(N^2) & Space Complexity: O(1)
+            int count = 0;
+            for(int i = 0; i < nums.length; i++){
+                HashSet<Integer> set = new HashSet<>();
+                for(int j = i; j < nums.length; j++){
+                    set.add(nums[j]);
+                    if(set.size() == k){
+                        count++;
+                    }
+                }
+            }
+            return count;
+        */
+
+        // Optimized Approach: Time Complexity: O(N) & Space Complxity: O(N)
+        /*Intituion: First we will count all the subarray which contains at max K element and then we will count all the subarray which contains at max K - 1 element and we will substract both to get the count of all subarray with K different Integers. */
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int i = 0, sum1 = 0, sum2 = 0;
+        for(int j = 0; j < nums.length; j++){
+            map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
+            while(map.size() > k){
+                map.put(nums[i], map.get(nums[i]) - 1);
+                if(map.get(nums[i]) == 0){
+                    map.remove(nums[i]);
+                }
+                i++;
+            }
+            sum1 += ((j - i) + 1);
+        }
+        i = 0;
+        HashMap<Integer, Integer> map2 = new HashMap<>();
+        for(int j = 0; j < nums.length; j++){
+            map2.put(nums[j], map2.getOrDefault(nums[j], 0) + 1);
+            while(map2.size() > k - 1){
+                map2.put(nums[i], map2.get(nums[i]) - 1);
+                if(map2.get(nums[i]) == 0){
+                    map2.remove(nums[i]);
+                }
+                i++;
+            }
+            sum2 += ((j - i) + 1);
+        }
+        return sum1 - sum2;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,2,1,2,3};
+        int k = 2;
+        System.out.println(subarraysWithKDistinct(nums, k));
+    }
+}
