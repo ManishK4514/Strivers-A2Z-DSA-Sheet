5c01724c077cb9f3beafcc42e168836f8c3173f6
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2022-12-01 16:13:07 +0530

    Added Iterative Preorder Traversal of Binary Tree.

---
+++ Binary Trees/Traversals/Iterative Preorder Traversal of Binary Tree.java
@@ -0,0 +1,68 @@
+/*
+   144. Binary Tree Preorder Traversal
+   Given the root of a binary tree, return the preorder traversal of its nodes' values.
+   
+   Example 1:
+   Input: root = [1,null,2,3]
+   Output: [1,2,3]
+
+   Example 2:   
+   Input: root = []
+   Output: []
+
+   Example 3:   
+   Input: root = [1]
+   Output: [1]
+*/
+
+import java.util.List;
+import java.util.Stack;
+import java.util.ArrayList;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int key){
+        this.data = key;
+    }
+}
+
+public class Iterative_Preorder_Traversal_of_Binary_Tree {
+    public static List<Integer> preorderTraversal(Node root) {
+        List<Integer> res = new ArrayList<>();
+        if(root == null) return res;
+        Stack<Node> st = new Stack<>();
+        st.push(root);
+        while(!st.empty()){
+            Node node = st.pop();
+            res.add(node.data);
+            if(node.right != null) st.push(node.right);
+            if(node.left != null) st.push(node.left);
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        /*
+            Real World Representation the below tree into the code.
+                    1
+                   /\
+                 2   3
+                / \  / \
+               4   5 6  7
+                  /     / \
+                 8    9    10
+        */
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.left.right.left = new Node(8);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        root.right.right.left = new Node(9);
+        root.right.right.right = new Node(10);
+        System.out.println(preorderTraversal(root));
+    }
+}
