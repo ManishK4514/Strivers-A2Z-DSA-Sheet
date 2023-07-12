3928ca84775bcef63ba8a0c9ed948ef29b990b2c
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2023-07-12 20:36:31 +0530

    Update Longest Increasing Subsequence (DP-41).

--- Dynamic Programming/DP on LIS/Longest Increasing Subsequence (DP-41).java
+++ Dynamic Programming/DP on LIS/Longest Increasing Subsequence (DP-41).java
@@ -1,7 +1,7 @@
 /*
     Q. Longest Increasing Subsequence | (DP-41)
 
-    Practice : https://leetcode.com/problems/longest-increasing-subsequence/description/
+    Practice: https://leetcode.com/problems/longest-increasing-subsequence/description/
 
     Given an integer array nums, return the length of the longest strictly increasing 
     subsequence.
@@ -27,41 +27,90 @@ public class Longest_Increasing_Subsequence {
     /*
        // Recursion
        public static int helper(int[] nums, int i, int prevIdx){
-           if(i < 0) return 0;
+           if(i == nums.length) return 0;
            
            int take = 0;
-           if(prevIdx == -1 || nums[prevIdx] > nums[i]){
-               take = 1 + helper(nums, i - 1, i);
+           if(prevIdx == -1 || nums[i] > nums[prevIdx]){
+               take = 1 + helper(nums, i + 1, i);
            }
-           int notTake = helper(nums, i - 1, prevIdx);
+           int notTake = helper(nums, i + 1, prevIdx);
            
            return Math.max(take, notTake);
+       }
+      
+       public static int lengthOfLIS(int[] nums) {
+           return helper(nums, 0, -1);
+       }
+    */
+   
+    /*
+       // Memoization
+       public static int helper(int[] nums, int i, int prevIdx, int[][] dp){
+           if(i == nums.length) return 0;
+   
+           if(dp[i][prevIdx + 1] != -1) return dp[i][prevIdx + 1];
+           
+           int take = 0;
+           if(prevIdx == -1 || nums[i] > nums[prevIdx]){
+               take = 1 + helper(nums, i + 1, i, dp);
+           }
+           int notTake = helper(nums, i + 1, prevIdx, dp);
+           
+           return dp[i][prevIdx + 1] = Math.max(take, notTake);
        }
    
        public static int lengthOfLIS(int[] nums) {
            int n = nums.length;
-           return helper(nums, n - 1, -1);
+           int[][] dp = new int[n][n + 1];
+           for(int[] it : dp) Arrays.fill(it, -1);
+           return helper(nums, 0, -1, dp);
        }
     */
-
-    // Memoization
-
-    public static int helper(int[] nums, int i, int prevIdx){
-        if(i < 0) return 0;
-        
-        int take = 0;
-        if(prevIdx == -1 || nums[prevIdx] > nums[i]){
-            take = 1 + helper(nums, i - 1, i);
-        }
-        int notTake = helper(nums, i - 1, prevIdx);
-        
-        return Math.max(take, notTake);
-    }
-
+   
+    /*
+       // Tabulation
+       public static int lengthOfLIS(int[] nums) {
+           int n = nums.length;
+           int[][] dp = new int[n + 1][n + 1];
+   
+           // second index shifted by 1
+   
+           for(int i = n - 1; i >= 0; i--){
+               for(int prevIdx = i - 1; prevIdx >= -1; prevIdx--){
+                   int take = 0;
+                   if(prevIdx == -1 || nums[i] > nums[prevIdx]){
+                       take = 1 + dp[i + 1][i + 1];
+                   }
+                   int notTake = dp[i + 1][prevIdx + 1];
+                   
+                   dp[i][prevIdx + 1] = Math.max(take, notTake);
+               }            
+           }
+   
+           return dp[0][-1 + 1];
+       }
+    */
+    
     public static int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        return helper(nums, n - 1, -1);
-    }
+        int[] ahead = new int[n + 1];
+        int[] curr = new int[n + 1];
+
+        for(int i = n - 1; i >= 0; i--){
+            for(int prevIdx = i - 1; prevIdx >= -1; prevIdx--){
+                int take = 0;
+                if(prevIdx == -1 || nums[i] > nums[prevIdx]){
+                    take = 1 + ahead[i + 1];
+                }
+                int notTake = ahead[prevIdx + 1];
+                
+                curr[prevIdx + 1] = Math.max(take, notTake);
+            }  
+            ahead = curr.clone();          
+        }
+
+        return ahead[-1 + 1];
+    }    
 
     public static void main(String[] args) {
         int[] nums = {10,9,2,5,3,7,101,18};
